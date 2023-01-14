9d76650b822d48f275d1596db3ec34543bdadc68
Nisam <nisam8104@gmail.com>
2023-01-15 00:28:44 +0530

    using FlatList,Pressable

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React, {useState} from 'react';
-import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
+import {
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  FlatList,
+  Pressable,
+} from 'react-native';
 
 const App = () => {
   const [resultText, setResultText] = useState('');
-  const [calcilationText, setCalculationText] = useState('');
+  const [calcilationText, setCalculationText] = useState(0);
 
   const onButtonClick = text => {
-    console.log(text);
     if (text == '=') {
       return calculation();
     }
@@ -25,13 +31,34 @@ const App = () => {
         resultText.toString().substring(0, resultText.length - 1),
       );
     }
-    console.log(text);
     if (operations.includes(resultText.toString().split('').pop())) return;
     setResultText(resultText + text);
   };
   const calculation = () => {
     setCalculationText(eval(resultText));
   };
+  const myData = [
+    {text: 1},
+    {text: 2},
+    {text: 3},
+    {text: 4},
+    {text: 5},
+    {text: 6},
+    {text: 7},
+    {text: 8},
+    {text: 9},
+    {text: '.'},
+    {text: 0},
+    {text: '='},
+  ];
+  const operationsData = [
+    {text: "DEL"},
+    {text: "AC"},
+    {text: "+"},
+    {text: "-"},
+    {text: "*"},
+    {text: "/"},
+  ]
   return (
     <View style={styles.container}>
       <View style={styles.result}>
@@ -42,125 +69,40 @@ const App = () => {
       </View>
       <View style={styles.button}>
         <View style={styles.numbers}>
-          <View style={styles.row}>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(1);
-              }}>
-              <Text style={styles.number}>1</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(2);
-              }}>
-              <Text style={styles.number}>2</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(3);
-              }}>
-              <Text style={styles.number}>3</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(4);
-              }}>
-              <Text style={styles.number}>4</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(5);
-              }}>
-              <Text style={styles.number}>5</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(6);
-              }}>
-              <Text style={styles.number}>6</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(7);
-              }}>
-              <Text style={styles.number}>7</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(8);
-              }}>
-              <Text style={styles.number}>8</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(9);
-              }}>
-              <Text style={styles.number}>9</Text>
-            </TouchableOpacity>
-          </View>
-          <View style={styles.row}>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick('.');
-              }}>
-              <Text style={styles.number}>.</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick(0);
-              }}>
-              <Text style={styles.number}>0</Text>
-            </TouchableOpacity>
-            <TouchableOpacity
-              onPress={() => {
-                onButtonClick('=');
-              }}>
-              <Text style={styles.number}>=</Text>
-            </TouchableOpacity>
-          </View>
+        <FlatList
+          data={myData}
+          numColumns={3}
+          renderItem={({item}) => {
+            return (
+              <Pressable
+                onPress={() => {
+                  onButtonClick(item.text);
+                }}>
+                <View style={{flex:1, padding:30}}>
+                  <Text style={styles.number}>{item.text}</Text>
+                </View>
+              </Pressable>
+            );
+          }}
+        />
         </View>
         <View style={styles.operations}>
-          <TouchableOpacity
-            onPress={() => {
-              onOperationClick('DEL');
-            }}>
-            <Text style={styles.operationButton}>DEL</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              onOperationClick('AC');
-            }}>
-            <Text style={styles.operationButton}>AC</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              onOperationClick('+');
-            }}>
-            <Text style={styles.operationButton}>+</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              onOperationClick('-');
-            }}>
-            <Text style={styles.operationButton}>-</Text>
-          </TouchableOpacity>
-          <TouchableOpacity
-            onPress={() => {
-              onOperationClick('*');
-            }}>
-            <Text style={styles.operationButton}>*</Text>
-          </TouchableOpacity>
-
-          <TouchableOpacity
-            onPress={() => {
-              onOperationClick('/');
-            }}>
-            <Text style={styles.operationButton}>/</Text>
-          </TouchableOpacity>
+          <FlatList
+            data={operationsData}
+            numColumns={1}
+            renderItem={({item}) => {
+              return (
+                <Pressable
+                  onPress={() => {
+                    onOperationClick(item.text);
+                  }}>
+                    <View style={{flex:1, padding:30}}>
+                    <Text style={styles.operationButton}>{item.text}</Text>
+                    </View>
+                </Pressable>
+              );
+            }}
+          />
         </View>
       </View>
     </View>
